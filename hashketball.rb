3dfe4d97019d70f72c0cc8470622435e8a7ceaa7
Ryan Daley <rdaley321@yahoo.com>
2018-11-10 15:13:57 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -168,7 +168,9 @@ def team_colors(string)
 end
 
 def team_names
-  teams = game_hash[:home][:team_name] + " " + game_hash[:away][:team_name]
+  teams = Array.new
+  teams << game_hash[:home][:team_name]
+  teams << game_hash[:away][:team_name]
   teams
 end
 
