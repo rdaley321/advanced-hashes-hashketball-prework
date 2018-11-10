a9319d3bc1fb17691f5527fd681b6f95db5404de
Ryan Daley <rdaley321@yahoo.com>
2018-11-10 15:43:23 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -196,5 +196,17 @@ def player_numbers(string)
 end
 
 def big_shoe_rebounds
-  
+  output = Hash.new
+  game_hash.each do |team,data|
+    data.each do |details,more_data|
+      if more_data.is_a?(Hash)
+        more_data.each do |key,value|
+          output[key] = value
+        end
+      end
+    end
+  end
+  puts output[string][:points]
 end
+
+big_shoe_rebounds
