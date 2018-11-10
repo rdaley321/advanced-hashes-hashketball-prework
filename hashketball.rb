745ce7d2ac7f91bb8b0f7dd20ba06ed45a689e36
Ryan Daley <rdaley321@yahoo.com>
2018-11-10 14:52:15 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -163,7 +163,7 @@ def team_colors(string)
   game_hash.each do |team,data|
     output_array << data
   end
-  puts output_array.select {|hash| hash[:team_name].include?("Brooklyn Nets")}
+  puts output_array.select{|hash| hash[:team_name].include?("Brooklyn Nets")}.flatten
 end
 
 team_colors("hey")
