0a647c3aa0e125cf79f5f7727abe9dbbe65e210f
Ryan Daley <rdaley321@yahoo.com>
2018-11-10 14:12:26 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -120,7 +120,7 @@ def num_points_scored(string)
   output = {}
   game_hash.each do |team,data|
     data.each do |details,more_data|
-      puts details[:team_name]
+      output[details] = more_data
     end
   end
 end
